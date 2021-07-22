62f5326436534217818804a83bf7095da158794c
KensieCarr <35310005+KensieCarr@users.noreply.github.com>
2021-07-22 13:55:23 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/HW1_Hello_World.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1_Hello_World.dir/main.cpp.o"
+  "HW1_Hello_World.pdb"
+  "HW1_Hello_World.exe"
+  "libHW1_Hello_World.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1_Hello_World.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
